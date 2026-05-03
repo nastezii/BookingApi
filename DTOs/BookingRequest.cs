8c0c3e72a48658705f84608544fe21ff9ad7b8ce
nastezii <nastiashalak@gmail.com>
2026-05-03 10:02:32 +0600

    add DTOs for auth and booking

---
+++ DTOs/BookingRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BookingApi.DTOs;
+
+public class BookingRequest
+{
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Description { get; set; } = "";
+}
