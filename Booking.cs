0980f10e872fda72e93be19d36a3cf0cb0e07a9e
nastezii <nastiashalak@gmail.com>
2026-05-03 09:42:57 +0600

    create User and Booking entities

---
+++ Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingApi.Models;
+
+public class Booking
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
+    public string Description { get; set; } = "";
+}
