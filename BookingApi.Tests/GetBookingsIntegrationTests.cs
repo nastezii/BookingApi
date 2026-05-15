eff28ef40cdfe59e5fc20d1973925ff731e3fbf0
nastezii <nastiashalak@gmail.com>
2026-05-15 19:58:31 +0600

    add integration test for bookings query endpoint

---
+++ BookingApi.Tests/GetBookingsIntegrationTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace BookingApi.Tests;
+
+public class GetBookingsIntegrationTests :
+    IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public GetBookingsIntegrationTests(
+        WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetBookings_ShouldReturnSuccessStatusCode()
+    {
+        var response =
+            await _client.GetAsync("/bookings");
+
+        Assert.Equal(
+            HttpStatusCode.Unauthorized,
+            response.StatusCode);
+    }
+}
