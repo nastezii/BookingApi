d82dffd2c876fa335126678a7afad5669e7582a1
nastezii <nastiashalak@gmail.com>
2026-05-15 18:20:43 +0600

    add create booking handler

---
+++ BookingApi.Application/Commands/Bookings/CreateBookingCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingApi.Application.Commands.Bookings;
+
+public class CreateBookingCommand
+{
+    public DateTime Start { get; set; }
+
+    public DateTime End { get; set; }
+
+    public Guid UserId { get; set; }
+}
---
+++ BookingApi.Application/Handlers/Bookings/CreateBookingHandler.cs
@@ -0,0 +1,39 @@
+﻿using BookingApi.Application.Commands.Bookings;
+using BookingApi.Domain.Entities;
+using BookingApi.Domain.Factories;
+using BookingApi.Domain.Repositories;
+
+namespace BookingApi.Application.Handlers.Bookings;
+
+public class CreateBookingHandler
+{
+    private readonly IBookingRepository _repository;
+
+    public CreateBookingHandler(IBookingRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Guid> Handle(CreateBookingCommand command)
+    {
+        var bookings = await _repository.GetAllAsync();
+
+        var hasConflict = bookings.Any(x =>
+            command.Start < x.End &&
+            command.End > x.Start);
+
+        if (hasConflict)
+        {
+            throw new Exception("Booking conflict");
+        }
+
+        var booking = BookingFactory.Create(
+            command.Start,
+            command.End,
+            command.UserId);
+
+        await _repository.AddAsync(booking);
+
+        return booking.Id;
+    }
+}
