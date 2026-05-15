ef4418499d76ea0e6a2ebfda2ba603d3585e1b59
nastezii <nastiashalak@gmail.com>
2026-05-15 18:23:49 +0600

    add get bookings handler

---
+++ BookingApi.Application/Handlers/Bookings/GetBookingsHandler.cs
@@ -0,0 +1,29 @@
+﻿using BookingApi.Application.Queries.Bookings;
+using BookingApi.Application.ReadModels.Bookings;
+using BookingApi.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingApi.Application.Handlers.Bookings;
+
+public class GetBookingsHandler
+{
+    private readonly AppDbContext _context;
+
+    public GetBookingsHandler(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<BookingReadModel>> Handle(GetBookingsQuery query)
+    {
+        return await _context.Bookings
+            .Select(x => new BookingReadModel
+            {
+                Id = x.Id,
+                Start = x.Start,
+                End = x.End,
+                UserId = x.UserId
+            })
+            .ToListAsync();
+    }
+}
