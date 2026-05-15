d633ddbdae2160d333f04d471faa372554f9ac58
nastezii <nastiashalak@gmail.com>
2026-05-15 19:57:03 +0600

    add unit tests for booking command handler

---
+++ BookingApi.Tests/CreateBookingHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using BookingApi.Application.Commands.Bookings;
+using BookingApi.Application.Handlers.Bookings;
+using BookingApi.Domain.Entities;
+using BookingApi.Domain.Repositories;
+using BookingApi.Domain.ValueObjects;
+using Moq;
+
+namespace BookingApi.Tests;
+
+public class CreateBookingHandlerTests
+{
+    [Fact]
+    public async Task Handle_ShouldThrowException_WhenBookingConflicts()
+    {
+        var repositoryMock =
+            new Mock<IBookingRepository>();
+
+        var existingBookings =
+            new List<Booking>
+            {
+                new Booking(
+                    1,
+                    new TimeRange(
+                        DateTime.Now,
+                        DateTime.Now.AddHours(2)),
+                    "Existing")
+            };
+
+        repositoryMock
+            .Setup(x => x.GetAllAsync())
+            .ReturnsAsync(existingBookings);
+
+        var handler =
+            new CreateBookingHandler(
+                repositoryMock.Object);
+
+        var command =
+            new CreateBookingCommand
+            {
+                UserId = 1,
+                Start = DateTime.Now.AddMinutes(30),
+                End = DateTime.Now.AddHours(1),
+                Description = "Test"
+            };
+
+        await Assert.ThrowsAsync<Exception>(() =>
+            handler.Handle(command));
+    }
+}
