438f130c75752cc12234466588123cdbd7bbe4c8
nastezii <nastiashalak@gmail.com>
2026-05-15 18:22:03 +0600

    add booking read model

---
+++ BookingApi.Application/ReadModels/Bookings/BookingReadModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingApi.Application.ReadModels.Bookings;
+
+public class BookingReadModel
+{
+    public Guid Id { get; set; }
+
+    public DateTime Start { get; set; }
+
+    public DateTime End { get; set; }
+
+    public Guid UserId { get; set; }
+}
